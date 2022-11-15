ce9e37992fbb2a28b6b2822286a02db6e444ff43
Banadesh <bannusbali@gmail.com>
2022-11-16 00:02:31 +0530

    Added new class

---
+++ src/main/java/com/example/git_demo1/Addition.java
@@ -0,0 +1,10 @@
+package com.example.git_demo1;
+
+public class Addition {
+    public static void main(String[] args) {
+        a = 10;
+        b=11;
+        c=a+b;
+        System.out.println(c);
+    }
+}
--- src/main/java/com/example/git_demo1/HelloWorld.java
+++ src/main/java/com/example/git_demo1/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.example.git_demo1;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
